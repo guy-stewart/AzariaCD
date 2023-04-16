d5e9cdcab2cf7fe48982aebb8ce0190da00573f9
Rick Gonzalez <rick.gonzalez68@gmail.com>
2023-04-15 23:14:17 -0400

    fixes and working on lever animation

--- data/sql/sounds.sql
+++ data/sql/sounds.sql
@@ -1,26 +1,31 @@
-drop table if exists sounds;
 
+drop table if exists sounds;
 create table sounds
 (
-    [name]      text unique primary key not null,
+    [name]      text,
     [value]     text,
-    [id]        int
+    [id]        int,   -- DEPRECATED / IGNORED
+    primary key ([name]) on conflict replace
 );
 
-insert into sounds values ('SOUND_BTNDRAG',	 'btndrag' , 1);
-insert into sounds values ('SOUND_BTNPRESS', 'btnpress', 2);
-insert into sounds values ('SOUND_POPUP',	 'lever'   , 3);
-insert into sounds values ('SOUND_CLUNK',	 'clunk'   , 4);
-insert into sounds values ('SOUND_FIRE',	 'fire'    , 5);
-insert into sounds values ('SOUND_CLICK',	 'choose'  , 6);
-insert into sounds values ('SOUND_HURT',	 'hurt'    , 7);
-insert into sounds values ('SOUND_BURBLE',	 'burble'  , 8);
-insert into sounds values ('SOUND_THUNDER',	 'thunder' , 9);
-insert into sounds values ('SOUND_EXPLODE',	 'explode' ,10);
-insert into sounds values ('SOUND_POP',      'pop'     ,11);
-insert into sounds values ('SOUND_ARROW',    'movingar',12);
-insert into sounds values ('SOUND_TON',	     'ton'     ,13);
-insert into sounds values ('SOUND_SLURP',    'slurp'   ,14);
-insert into sounds values ('SOUND_SPIT',     'spit'    ,15);
-insert into sounds values ('SOUND_FUZZBUZZ', 'fuzzbuzz',16);
-insert into sounds values ('SOUND_BUZZFUZZ', 'buzzfuzz',17);
+insert into sounds values ('EFFECT_CLICK','wrong value please repalce',0);
+insert into sounds values ('EFFECT_CLICK','pop',0);
+insert into sounds values ('SOUND_NARRATION','narrative',0);
+
+insert into sounds values ('SOUND_BTNDRAG', 	'btndrag',0);
+insert into sounds values ('SOUND_BTNPRESS',	'btnpress',0);
+insert into sounds values ('SOUND_POPUP',   	'lever',0);
+insert into sounds values ('SOUND_CLUNK',   	'clunk',0);
+insert into sounds values ('SOUND_FIRE',    	'fire',0);
+insert into sounds values ('SOUND_CLICK',   	'choose',0); 
+insert into sounds values ('SOUND_HURT',    	'hurt',0);
+insert into sounds values ('SOUND_BURBLE',  	'burble',0);  
+insert into sounds values ('SOUND_THUNDER', 	'thunder',0);   
+insert into sounds values ('SOUND_EXPLODE', 	'explode',0);  
+insert into sounds values ('SOUND_POP',     	'pop',0);
+insert into sounds values ('SOUND_ARROW',   	'movingar',0); 
+insert into sounds values ('SOUND_TON',     	'ton',0);
+insert into sounds values ('SOUND_SLURP',   	'slurp',0);  
+insert into sounds values ('SOUND_SPIT',    	'spit',0);
+insert into sounds values ('SOUND_FUZZBUZZ',	'fuzzbuzz',0);   
+insert into sounds values ('SOUND_BUZZFUZZ',	'buzzfuzz',0);  
