430321aa1a3fa7703a4abcad97f5ff99a4331c64
Rick Gonzalez <rick.gonzalez68@gmail.com>
2023-04-19 23:04:31 -0400

    finishes for lever area maproom and btn

--- data/sql/lever.sql
+++ data/sql/lever.sql
@@ -97,6 +97,8 @@ INSERT INTO "main"."transitions" ("name", "state", "new_state", "opcode", "param
 ('M_STATESCALE', '11', '0', 'SHOW', '0', '0');
 
 -- Scene 21 Plants and Rocks
+delete from "main"."objects" where object = 'IDD_MAPBTN';
+INSERT INTO "main"."objects" ("object", "object_id", "class", "class_id", "icon", "cursor", "actor") VALUES ('IDD_MAPBTN', '8572', 'IDC_NULL', '0', 'mapbtn', 'mapbtn', 'mapbtn');
 
 delete from  "main"."machines" where name = 'S21_m1BERRYTREE';
 delete from  "main"."machines" where name = 'S21_p3BERRYTREE';
@@ -114,24 +116,34 @@ delete from  "main"."machines" where name = 'S21_o4BERRYTREE';
 delete from  "main"."machines" where name = 'S21_o5BERRYTREE';
 delete from  "main"."machines" where name = 'S21_o6BERRYTREE';
 delete from  "main"."machines" where name = 'S21_ROCKBIN1';
+delete from  "main"."machines" where name = 'S21_MAPBIN';
+delete from  "main"."machines" where name = 'S21_MAPTEXT';
 
 INSERT INTO "main"."machines" ("id", "name", "view_id", "view_name", "left", "top", "right", "bottom", "local_visible", "dfa_name", "wip1_name", "wip1_value", "wip2_name", "wip2_value", "wip3_name", "wip3_value", "wip4_name", "wip4_value") 
  VALUES 
  
     ('8449', 'S21_m1BERRYTREE', '8100', 'IDV_N2A', '1830', '213', '1900', '273', '2', 'M_PLANTBIN', 'IDD_FERNLEAF', '8480', 'IDS_FERN', '8497', '60', '60', '', '0'),
     ('8463', 'S21_p3BERRYTREE', '8100', 'IDV_N2A', '708', '220', '758', '278', '2', 'M_PLANTBIN', 'IDD_WFLWRSPT', '8487', 'IDS_WFLOWER', '8501', '60', '60', '', '0'),
+   
     ('8450', 'S21_m2BERRYTREE', '8101', 'IDV_N2B', '2015', '166', '2115', '314', '2', 'M_BIN', 'IDD_REDBERRY', '8486', '', '0', '', '0', '', '0'),
     ('8454', 'S21_m6BERRYTREE', '8101', 'IDV_N2B', '570', '190', '760', '230', '2', 'M_PLANTBIN', 'IDD_YFLWRSPT', '8488', 'IDS_YFLOWER', '8502', '60', '60', '', '0'),
     ('8462', 'S21_p2BERRYTREE', '8101', 'IDV_N2B', '2471', '230', '2531', '290', '2', 'M_PLANTBIN', 'IDD_PFLWRSPT', '8483', 'IDS_PFLOWER', '8500', '60', '60', '', '0'),
     ('8464', 'S21_p4BERRYTREE', '8101', 'IDV_N2B', '1962', '234', '2022', '294', '2', 'M_PLANTBIN', 'IDD_GLEAFSPT', '8481', 'IDS_GRNPLANT', '8498', '60', '60', '', '0'),
+    
     ('8451', 'S21_m3BERRYTREE', '8102', 'IDV_N2C', '572', '212', '632', '272', '2', 'M_PLANTBIN', 'IDD_PFLWRSPT', '8483', 'IDS_PFLOWER', '8500', '60', '60', '', '0'),
     ('8452', 'S21_m4BERRYTREE', '8102', 'IDV_N2C', '219', '219', '279', '279', '2', 'M_PLANTBIN', 'IDD_PFLWRSPT', '8483', 'IDS_PFLOWER', '8500', '60', '60', '', '0'),
     ('8453', 'S21_m5BERRYTREE', '8102', 'IDV_N2C', '61', '161', '235', '209', '2', 'M_PLANTBIN', 'IDD_OMUSHSPT', '8482', 'IDS_OMUSH', '8499', '60', '60', '', '0'),
-    ('8455', 'S21_o1BERRYTREE', '8103', 'IDV_N2D2', '1679', '209', '1720', '269', '2', 'M_PLANTBIN', 'IDD_OMUSHSPT', '8482', 'IDS_OMUSH', '8499', '60', '60', '', '0'),
+   
+    ('8455', 'S21_o1BERRYTREE', '8103', 'IDV_N2D2', '1711', '209', '1894', '269', '2', 'M_PLANTBIN', 'IDD_OMUSHSPT', '8482', 'IDS_OMUSH', '8499', '60', '60', '', '0'),
     ('8456', 'S21_o2BERRYTREE', '8103', 'IDV_N2D2', '429', '229', '489', '289', '2', 'M_PLANTBIN', 'IDD_GLEAFSPT', '8481', 'IDS_GRNPLANT', '8498', '60', '60', '', '0'),
-    ('8457', 'S21_o3BERRYTREE', '8103', 'IDV_N2D2', '1180', '133', '1280', '291', '2', 'M_BIN', 'IDD_BLUEBERRY', '8485', '', '0', '', '0', '', '0'),
     ('8458', 'S21_o4BERRYTREE', '8103', 'IDV_N2D2', '2030', '201', '2090', '261', '2', 'M_PLANTBIN', 'IDD_YFLWRSPT', '8488', 'IDS_YFLOWER', '8502', '60', '60', '', '0'),
     ('8459', 'S21_o5BERRYTREE', '8103', 'IDV_N2D2', '2100', '212', '2160', '272', '2', 'M_PLANTBIN', 'IDD_PFLWRSPT', '8483', 'IDS_PFLOWER', '8500', '60', '60', '', '0'),
     ('8460', 'S21_o6BERRYTREE', '8103', 'IDV_N2D2', '2170', '195', '2230', '255', '2', 'M_PLANTBIN', 'IDD_PFLWRSPT', '8483', 'IDS_PFLOWER', '8500', '60', '60', '', '0'),
+    ('8470', 'S21_ROCKBIN1', '8102', 'IDV_N2C', '1081', '223', '1120', '255', '2', 'M_PLANTBIN', 'IDS_ROCK', '5892', 'IDD_ROCKBIN', '5898','60', '60', '', '0'),
 
-    ('8460', 'S21_ROCKBIN1', '8102', 'IDV_N2C', '1081', '223', '1120', '255', '2', 'M_PLANTBIN', 'IDD_ROCKBIN', '5898', 'IDS_ROCK', '5892', '60', '60', '', '0');
+    -- Scene 21 Maproom Map bin
+    --8471 ->
+
+   ('8471', 'S21_MAPBIN', '8106', 'IDV_MAPROOM', '2708', '7', '2926', '179', '2', 'M_BIN', 'IDD_MAPBTN', '8572', '', '0', '', '0', '', '0'),
+   ('8472', 'S21_MAPTEXT', '8106', 'IDV_MAPROOM', '1441', '2', '1810','200', '2', 'M24_EYETEXT', 'IDS_MAPENG', '5899', '', '0', '', '0', '', '0');
+   
