fc9a121c465a0a53898f87fe126ee631bb5227f3
Rick Gonzalez <rick.gonzalez68@gmail.com>
2023-04-22 11:23:01 -0400

    thought i'd pushed alreday

--- data/sql/S11_Fishbeach.sql
+++ data/sql/S11_Fishbeach.sql
@@ -34,18 +34,17 @@ VALUES
 ('4387', 'S11_POLESTANDc', '4356', 'IDV_FH1PAN', '2800', '120', '2850', '300', '0', 'M_POLESTAND', '', '0', '', '0', '', '0', '', '0'),
 ('4388', 'S11_POLESTANDd', '4356', 'IDV_FH1PAN', '2850', '120', '2900', '300', '0', 'M_POLESTAND', '', '0', '', '0', '', '0', '', '0'),
 
-('4400', 'S11_FISHSTATIONa', '4356', 'IDV_FH1PAN', '600', '110', '700', '300', '0', 'M_FISHSTATION', '', '0', '', '0', '', '0', '', '0'),
-('4401', 'S11_FISHSTATIONb', '4356', 'IDV_FH1PAN', '700', '110', '800', '300', '0', 'M_FISHSTATION', '', '0', '', '0', '', '0', '', '0'),
-('4402', 'S11_FISHSTATIONc', '4356', 'IDV_FH1PAN', '800', '110', '900', '300', '0', 'M_FISHSTATION', '', '0', '', '0', '', '0', '', '0'),
-('4403', 'S11_FISHSTATIONd', '4356', 'IDV_FH1PAN', '900', '110', '1000', '300', '0', 'M_FISHSTATION', '', '0', '', '0', '', '0', '', '0'),
-('4404', 'S11_FISHSTATIONe', '4356', 'IDV_FH1PAN', '1000', '110', '1100', '300', '0', 'M_FISHSTATION', '', '0', '', '0', '', '0', '', '0'),
-('4405', 'S11_FISHSTATIONf', '4356', 'IDV_FH1PAN', '1100', '110', '1200', '300', '0', 'M_FISHSTATION', '', '0', '', '0', '', '0', '', '0'),
-('4406', 'S11_FISHSTATIONg', '4356', 'IDV_FH1PAN', '1200', '110', '1300', '300', '0', 'M_FISHSTATION', '', '0', '', '0', '', '0', '', '0'),
-('4407', 'S11_FISHSTATIONh', '4356', 'IDV_FH1PAN', '1300', '110', '1400', '300', '0', 'M_FISHSTATION', '', '0', '', '0', '', '0', '', '0'),
+('4405', 'S11_FISHSTATIONa', '4356', 'IDV_FH1PAN', '1100', '110', '1200', '300', '0', 'M_FISHSTATION', '', '0', '', '0', '', '0', '', '0'),
+('4406', 'S11_FISHSTATIONb', '4356', 'IDV_FH1PAN', '1200', '110', '1300', '300', '0', 'M_FISHSTATION', '', '0', '', '0', '', '0', '', '0'),
+('4407', 'S11_FISHSTATIONc', '4356', 'IDV_FH1PAN', '1300', '110', '1400', '300', '0', 'M_FISHSTATION', '', '0', '', '0', '', '0', '', '0'),
+('4400', 'S11_FISHSTATIONd', '4356', 'IDV_FH1PAN', '1600', '110', '1700', '300', '0', 'M_FISHSTATION', '', '0', '', '0', '', '0', '', '0'),
+('4401', 'S11_FISHSTATIONe', '4356', 'IDV_FH1PAN', '1700', '110', '1800', '300', '0', 'M_FISHSTATION', '', '0', '', '0', '', '0', '', '0'),
+('4402', 'S11_FISHSTATIONf', '4356', 'IDV_FH1PAN', '1800', '110', '1900', '300', '0', 'M_FISHSTATION', '', '0', '', '0', '', '0', '', '0'),
 
 ('4448', 'S11_GRUBBIN0', '4358', 'IDV_GRBDRT2', '20', '8', '262', '218', '2', 'M_OBJECTBIN', 'IDD_BAIT0', '4400', '', '0', '', '0', '', '0'),
-('4433', 'S11_LOGBINB', '4403', '', '2833', '168', '3126', '265', '2', 'M_OBJECTBIN', 'IDD_LOG', '4464', '', '0', '', '0', '', '0');
+('4433', 'S11_LOGBINB', '4403', '', '2833', '168', '3126', '265', '2', 'M_OBJECTBIN', 'IDD_LOG', '4464', '', '0', '', '0', '', '0'),
 
+('4450', 'S11_SHOREBIN', '4403', '', '2833', '168', '3126', '265', '2', 'M_OBJECTBIN', 'IDD_LOG', '4464', '', '0', '', '0', '', '0');
 
 -- Bait stations combining with fish stations
 
@@ -56,7 +55,6 @@ INSERT INTO "main"."transitions" ("name", "state", "new_state", "opcode", "param
         ('M_FISHSTATION', '2', '50', 'DRAG', '0', 'IDD_BUCKE'),
         ('M_FISHSTATION', '2', '3', 'DROP', '0', '0'),
         ('M_FISHSTATION', '2', '0', 'GRAB', '0', '0'),
-
         ('M_FISHSTATION', '3', '4', 'IS_A', 'WOBJECT', 'ISA_BAITEDPOLE'),
         ('M_FISHSTATION', '3', '30', 'Z_EPSILON', '0', '0'), --else get baited
         ('M_FISHSTATION', '4', '5', 'SHOW', '0', 'IDS_POLE1LCL'), --baited cast
@@ -73,7 +71,6 @@ INSERT INTO "main"."transitions" ("name", "state", "new_state", "opcode", "param
         ('M_FISHSTATION', '15', '16', 'XIM', 'WOBJECT', 'WPARM'),
         ('M_FISHSTATION', '16', '17', 'SHOW', 'WOBJECT', ''),
         ('M_FISHSTATION', '17', '0', 'GRAB', '0', '0'),
-
         ('M_FISHSTATION', '30', '31', 'SHOW', '0', 'IDS_POLE1LCU'),
         ('M_FISHSTATION', '31',	'32', 'MOV','WPARM','WOBJECT'),
         ('M_FISHSTATION', '32', '33', 'C_ACCEPT', '0', 'ISA_BAIT'),
@@ -85,4 +82,7 @@ INSERT INTO "main"."transitions" ("name", "state", "new_state", "opcode", "param
         ('M_FISHSTATION', '50', '2', 'HANDOFF', '0', 'IDD_BUCKF');
 
 
-
+delete from machines where name = 'S11_SHOREBIN';
+delete from machines where name = 'S11_SHORE';
+INSERT INTO "main"."machines" ("id", "name", "view_id", "view_name", "left", "top", "right", "bottom", "local_visible", "dfa_name", "wip1_name", "wip1_value", "wip2_name", "wip2_value", "wip3_name", "wip3_value", "wip4_name", "wip4_value") VALUES ('4460', 'S11_SHORE', '4356', 'IDV_FH1PAN', '980', '219', '1000', '281', '2', 'M_PORTAL', 'IDV_SHORE', '4360', 'IDV_SHORE', '4360', '60', '0', '', '0');
+INSERT INTO "main"."machines" ("id", "name", "view_id", "view_name", "left", "top", "right", "bottom", "local_visible", "dfa_name", "wip1_name", "wip1_value", "wip2_name", "wip2_value", "wip3_name", "wip3_value", "wip4_name", "wip4_value") VALUES ('4461', 'S11_SHOREBIN', '4360', 'IDV_SHORE', '129', '87', '321', '213', '0', 'M_ANIBIN', 'IDS_FISH', '0', 'IDD_BAIT3', '0', '60', '0', '', '0');
