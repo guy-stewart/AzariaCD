0499a0cf06df45dc80ac3187419536f865da0a5a
Rick Gonzalez <rick.gonzalez68@gmail.com>
2023-06-22 21:42:02 -0400

    Updated eyeofgod and moon table recording

--- data/sql/S0_Update_Recording.sql
+++ data/sql/S0_Update_Recording.sql
@@ -1,7 +1,7 @@
 
 delete from games;
 
-
+------------DRYPIT COMPLETION
 delete from machines where name = 'S10_DRYPIT';
 delete from machines where name = 'S10_FLOWER';
 insert into machines values (0x1000,'S10_DRYPIT',0x1002,'IDV_SCN10PIT',30,205,379,300,0,'M10_DRYPIT','', '', '', '' );
@@ -53,7 +53,7 @@ insert into transitions ([automaton], [state], [new_state], [opcode], [param_1],
     SHOW(0,IDS_FLOPN1);
     ANIMATE(0,V_REVERSE);','');
 
-------------Caves
+------------CAVES COMPLETION
 --M25_OPNDOOR
 delete from "main"."machines" where [name] like 'S25_OPN%';
 delete from "main"."machines" where [name] like 'S25_EXIT%';
@@ -80,3 +80,61 @@ insert into transitions ([automaton], [state], [new_state], [opcode], [param_1],
 
 ('M25_EXITDOOR','0','1','CLICK','0','0','',''),
 ('M25_EXITDOOR','1','0','LOADVIEW','WIP1','','','');
+
+
+------------EYE OF GOD COMPLETEION
+
+delete from transitions where automaton = 'M24_BIGEYE';
+delete from transitions where automaton = 'M_MEMOBJ';
+INSERT INTO "main"."transitions" ("automaton", "state", "new_state", "opcode", "param_1", "param_2", "code", "guard") VALUES 
+('M24_BIGEYE', '0', '1', 'MOV', 'WSPRITE', 'WIP1', '', ''),
+('M24_BIGEYE', '1', '2', 'ASHOW', 'WSPRITE', 'V_LOOP', '', ''),
+('M24_BIGEYE', '2', '5', 'ANIMATE', '0', 'V_LOOP', '', ''),
+('M24_BIGEYE', '5', '6', 'SIGNAL', 'WIP2', 'SIG_SHOW', '', ''),
+('M24_BIGEYE', '6', '7', 'SIGNAL', 'WIP3', 'SIG_SHOW', '', ''),
+('M24_BIGEYE', '7', '0', 'CLICK', '', '', '', ''),
+('M24_BIGEYE', '7', '10', 'WAIT', '0', '0', '', ''),
+('M24_BIGEYE', '10', '50', 'IFSTATEi', '0', 'S24_EYEPLATE1', '', ''),
+('M24_BIGEYE', '10', '50', 'IFSTATEi', '0', 'S24_EYEPLATE2', '', ''),
+('M24_BIGEYE', '10', '50', 'IFSTATEi', '0', 'S24_EYEPLATE3', '', ''),
+('M24_BIGEYE', '10', '50', 'IFSTATEi', '0', 'S24_EYESPELL1', '', ''),
+('M24_BIGEYE', '10', '50', 'IFSTATEi', '0', 'S24_EYESPELL3', '', ''),
+('M24_BIGEYE', '10', '20', 'Z_EPSILON', '', '', '', ''),
+('M24_BIGEYE', '20', '30', 'ASHOW', '0', 'IDS_BIGEYE1', '', ''),
+('M24_BIGEYE', '30', '30', 'GRAB', '0', 'IDD_EYEWAND', 'SIGNALi(SIG_COMPLETE,REC_EYEOG);', ''),
+('M24_BIGEYE', '30', '50', 'WAIT', '0', 'SIG_BOMB', '', ''),
+('M24_BIGEYE', '50', '51', 'SHOW', '0', '0', '', ''),
+('M24_BIGEYE', '51', '52', 'SIGNAL', 'WIP2', 'SIG_HIDE', '', ''),
+('M24_BIGEYE', '52', '0', 'SIGNAL', 'WIP3', 'SIG_HIDE', '', ''),
+
+
+------------HIDDEN NATURE AREA DISCOVERY
+
+------------MOON ALTAR TABLE 
+
+('M_MEMOBJ','0','1','O_ACCEPT','WIP1','','',''),
+('M_MEMOBJ','1','2','DROP','0','','',''),
+('M_MEMOBJ','11','12','SIGNALi','SIG_SHOW','S27_MEMSTONE1','',''),
+('M_MEMOBJ','12','13','SIGNALi','SIG_SHOW','S27_MEMSTONE2','',''),
+('M_MEMOBJ','13','14','SIGNALi','SIG_SHOW','S27_MEMSTONE3','',''),
+('M_MEMOBJ','14','15','SIGNALi','SIG_SHOW','S27_MEMSTONE4','',''),
+('M_MEMOBJ','15','16','SIGNALi','SIG_SHOW','S27_MEMSTONE5','',''),
+('M_MEMOBJ','16','17','SIGNALi','SIG_SHOW','S27_MEMSTONE6','',''),
+('M_MEMOBJ','17','18','SIGNALi','SIG_SHOW','S27_MEMSTONE7','',''),
+('M_MEMOBJ','18','20','GRAB','WIP1','','',''),
+('M_MEMOBJ','2','3','MOV','WSPRITE','WIP3','',''),
+('M_MEMOBJ','20','21','CLEAR','WOBJECT','','',''),
+('M_MEMOBJ','21','22','CLEAR','WPARM','','',''),
+('M_MEMOBJ','22','23','CLEAR','WSPRITE','','',''),
+('M_MEMOBJ','23','30','SHOW','','','',''),
+('M_MEMOBJ','3','4','SHOW','WSPRITE','','',''),
+('M_MEMOBJ','30','31','SIGNALi','SIG_HIDE','S27_MEMSTONE1','',''),
+('M_MEMOBJ','31','32','SIGNALi','SIG_HIDE','S27_MEMSTONE2','',''),
+('M_MEMOBJ','32','33','SIGNALi','SIG_HIDE','S27_MEMSTONE3','',''),
+('M_MEMOBJ','33','34','SIGNALi','SIG_HIDE','S27_MEMSTONE4','',''),
+('M_MEMOBJ','34','35','SIGNALi','SIG_HIDE','S27_MEMSTONE5','',''),
+('M_MEMOBJ','35','36','SIGNALi','SIG_HIDE','S27_MEMSTONE6','',''),
+('M_MEMOBJ','36','0','SIGNALi','SIG_HIDE','S27_MEMSTONE7','',''),
+('M_MEMOBJ','4','5','ASSIGN','BPARM','WIP1','',''),
+('M_MEMOBJ','5','11','IFSTATE','18','WIP4','SIGNALi(SIG_COMPLETE,REC_MOONTBL);',''),
+('M_MEMOBJ','5','18','Z_EPSILON','','','','');
