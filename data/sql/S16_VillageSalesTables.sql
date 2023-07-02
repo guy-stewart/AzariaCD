02ba05e2314e47ac86b30c3ab2319ef1ac07b749
Rick Gonzalez <rick.gonzalez68@gmail.com>
2023-07-01 21:20:46 -0400

    FIX TYPO!

--- data/sql/S16_VillageSalesTables.sql
+++ data/sql/S16_VillageSalesTables.sql
@@ -1,4 +1,4 @@
-delete from games;
+drop table games;
 
 delete from spr_names where [name] like 'IDS_STAMP%';
 insert into spr_names values ('IDS_STAMP','STAMP','25050');
@@ -8,7 +8,7 @@ insert into spr_names values ('IDS_STAMP','STAMP','25050');
 delete from objects where object = 'IDD_STAMP';
 
 insert into objects values
-('IDD_STAMP',25050,'IDC_NULL','STAMP','STAPM','STAMP');
+('IDD_STAMP',25050,'IDC_NULL','STAMP','STAMP','STAMP');
 
 
 
