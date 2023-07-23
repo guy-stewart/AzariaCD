17bdc0d1831ad8b2ed0ce3308abb7050b047d369
Rick Gonzalez <rick.gonzalez68@gmail.com>
2023-07-23 11:01:30 -0400

    video work for end game

--- data/sql/S29_Kamioza.sql
+++ data/sql/S29_Kamioza.sql
@@ -1,18 +1,26 @@
 delete from games;
 
 
-delete from "main"."views" where [view_name] like 'IDV_KAMIOZA';
+
+delete from spr_names where name = 'IDS_REACH';
+INSERT INTO "main"."spr_names" ("name", "value", "id") VALUES ('IDS_REACH','reach','1640');
+
+delete from "main"."views" where [view_name] like 'IDV_KAMIOZ%';
 INSERT INTO "main"."views" ("view_id", "view_name", "Z", "backgroundAudio", "locator_view", "behavior_id", "portal_filename", "surface_filename") 
 VALUES 
-('9801', 'IDV_KAMIOZA', '1', '0', '1', '6', 'surround.vct', 'KAMIOZA');
+('9801', 'IDV_KAMIOZA', '1', '0', '1', '6', 'surround.vct', 'KAMIOZA'),
+('9750', 'IDV_KAMIOZAREACH', '1', '3', '1', '1', 'smlpanel.vct', 'KAMIOZAREACH');
 
 
+delete from "main"."machines" where [name] like 'S29_KAM%';
 INSERT INTO "main"."machines" ("id", "name", "view_id", "view_name", "left", "top", "right", "bottom", "local_visible", "dfa_name", "wip1_name", "wip2_name","wip3_name",  "wip4_name") 
 VALUES 
 ('9920', 'S29_KAMALTAR', '9801', 'IDV_KAMIOZA', '3129', '180', '3186', '200', '0','M_KAMALTAR','S29_KAMRAIN','','', ''),
-('9921', 'S29_KAMRAIN',  '9801', 'IDV_KAMIOZA', '3010', '10', '55', '300', '0','M_KAMRAIN','','','', '');
+('9921', 'S29_KAMRAIN',  '9801', 'IDV_KAMIOZA', '3010', '20', '55', '300', '0','M_KAMRAIN','','','', ''),
+('9922', 'S29_KAMREACH',  '9750', 'IDV_KAMIOZAREACH', '0', '0', '400', '300', '0','M_KAMREACH','','','', '');
 
-delete from transitions where automaton = 'M_KAMALTAR';
+
+delete from transitions where [automaton] like 'M_KAM%';
 INSERT INTO "main"."transitions" ("automaton", "state", "new_state", "opcode", "param_1", "param_2", "code","guard")
 VALUES 
 ('M_KAMALTAR','0','5','ACCEPT','','IDD_SEED','',''),
@@ -23,6 +31,11 @@ VALUES
     SHOW();
 ',''),
 
-
 ('M_KAMRAIN','0','1','WAIT','','SIG_SHOW','',''),
-('M_KAMRAIN','1','0','VIDEO', '', 'IDS_RAIN', '','');
+('M_KAMRAIN','1','0','VIDEO', '', 'IDS_RAIN', '
+    LOADVIEW(S29_KAMIOZAREACH);
+',''),
+
+
+('M_KAMREACH','0','1','Z_EPSILON', '', '', '',''),
+('M_KAMREACH','1','0','VIDEO', '', 'IDS_REACH', '','');
