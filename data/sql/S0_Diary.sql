00d5d812a4b1a7646849793c99724cd87b8d1d53
Rick Gonzalez <rick.gonzalez68@gmail.com>
2023-10-04 22:49:45 -0400

    Updates to eye info for objects and diaries

--- data/sql/S0_Diary.sql
+++ data/sql/S0_Diary.sql
@@ -1,92 +1,95 @@
-
-delete from games;
-
-----RESOURCES 
-
-delete from isa where [class] like 'ISA_DIARY_%';
-INSERT INTO "main"."isa" ("class", "member")
-VALUES 
-('ISA_DIARY', 'IDD_DIARY1');
+----  MIGRATED TO S0_EYEINFOV2!
 
 
+-- delete from games;
 
-drop table if exists diary;
--- 'id' is used by the card index cartridge on the card disbursement machine in the city.
-create table diary (object text, control text, content text);
-insert into diary values
-( 'IDD_DIARY1', 'ID_PARCHBIGTXT' ,'
-        I leave this here in case something happens
-        46 Zan, 192978
+-- ----RESOURCES 
+
+-- delete from isa where [class] like 'ISA_DIARY_%';
+-- INSERT INTO "main"."isa" ("class", "member")
+-- VALUES 
+-- ('ISA_DIARY', 'IDD_DIARY1');
+
+
+
+-- drop table if exists objectInfo;
+-- drop table if exists diary;
+-- -- 'id' is used by the card index cartridge on the card disbursement machine in the city.
+-- create table objectInfo (object text, control text, content text);
+-- insert into objectInfo values
+-- ( 'IDD_DIARY1', 'ID_PARCHBIGTXT' ,'
+--         I leave this here in case something happens
+--         46 Zan, 192978
         
-        I Onam, archiologist of Niret 
-        have  found the sacred map of our forefathers
-        that grants power over the sky.
+--         I Onam, archiologist of Niret 
+--         have  found the sacred map of our forefathers
+--         that grants power over the sky.
         
-        Although their language still comfounds me!
+--         Although their language still comfounds me!
 
-        Now, I shall continue the quest spurred on 
-        by our great discovery of a promise written in stone.
-        I speak of course of the stone rubbing unearthed 
-        outside of Etnoc, that hideous city of great 
-        sadness and tragedy.
+--         Now, I shall continue the quest spurred on 
+--         by our great discovery of a promise written in stone.
+--         I speak of course of the stone rubbing unearthed 
+--         outside of Etnoc, that hideous city of great 
+--         sadness and tragedy.
 
-        It is the Niret way of spirit that makes us so 
-        much better than those of Etnoc.
+--         It is the Niret way of spirit that makes us so 
+--         much better than those of Etnoc.
 
-        ....to take the seed of Azaria and plant it
-         on the altar of seven moons.
+--         ....to take the seed of Azaria and plant it
+--          on the altar of seven moons.
         
-        I imagine the seed as some relic that existed 
-        before the rains... I must find it! Something that
-        contains this seed.
+--         I imagine the seed as some relic that existed 
+--         before the rains... I must find it! Something that
+--         contains this seed.
 
-        I shall now see for myself and fill my spirit with joy - 
-        Searching for something to plant on some altar!'),
+--         I shall now see for myself and fill my spirit with joy - 
+--         Searching for something to plant on some altar!'),
 
-( 'IDD_DIARY2', 'IDV_PARCHBIG' ,'Placeholder1'),
-( 'IDD_DIARY3', 'IDV_PARCHBIG' ,'Placeholder2'),
-( 'IDD_DIARY4', 'IDV_PARCHBIG' ,'Placeholder3');
+-- ( 'IDD_DIARY2', 'IDV_PARCHBIG' ,'Placeholder1'),
+-- ( 'IDD_DIARY3', 'IDV_PARCHBIG' ,'Placeholder2'),
+-- ( 'IDD_DIARY4', 'IDV_PARCHBIG' ,'Placeholder3');
 
--- predicate diary(id,content);
--- in this case (R_WPARM) but any pointed to set parameter or literal
--- cardmach(R_WPARM,?WOBJECT);
+-- -- predicate diary(id,content);
+-- -- in this case (R_WPARM) but any pointed to set parameter or literal
+-- -- cardmach(R_WPARM,?WOBJECT);
 
-delete from views where [view_name] like 'IDV_PARCHBIG%';
-INSERT INTO "main"."views" ("view_id", "view_name", "Z", "backgroundAudio", "locator_view", "behavior_id", "portal_filename", "surface_filename") 
-VALUES 
-('8710', 'IDV_PARCHBIG', '1', '0', '1', '2', 'wdepanel.vct', 'parchbig');
+-- delete from views where [view_name] like 'IDV_PARCHBIG%';
+-- INSERT INTO "main"."views" ("view_id", "view_name", "Z", "backgroundAudio", "locator_view", "behavior_id", "portal_filename", "surface_filename") 
+-- VALUES 
+-- ('8710', 'IDV_PARCHBIG', '1', '0', '1', '2', 'wdepanel.vct', 'parchbig');
 
-delete from spr_names where name = 'IDS_PARCHBIGBK';
-INSERT INTO "main"."spr_names" ("name", "value", "id") 
-VALUES 
-('IDS_PARCHBIGBK', 'parchbigbk', '40650');
+-- delete from spr_names where name = 'IDS_PARCHBIGBK';
+-- INSERT INTO "main"."spr_names" ("name", "value", "id") 
+-- VALUES 
+-- ('IDS_PARCHBIGBK', 'parchbigbk', '40650');
 
 
-delete from controls where id = 'ID_PARCHBIGTXT';
-insert into controls values
-('IDV_PARCHBIG','ID_PARCHBIGTXT','LABEL','IDS_PARCHBIGBK','',46, 55, 0,'','', 'IDS_FONTTNB16', 0x010101,'');
+-- delete from controls where id = 'ID_PARCHBIGTXT';
+-- insert into controls values
+-- ('IDV_PARCHBIG','ID_PARCHBIGTXT','LABEL','IDS_PARCHBIGBK','',46, 55, 0,'','', 'IDS_FONTTNB16', 0x010101,'');
 
-delete from machines where name = 'PARCHBIG_WAITER';
-INSERT INTO "main"."machines" ("id", "name", "view_id", "view_name", "left", "top", "right", "bottom", "local_visible", "dfa_name", "wip1_name", "wip2_name", "wip3_name", "wip4_name") 
-VALUES 
-('15570', 'PARCHBIG_WAITER', '8710', 'IDV_PARCHBIG', '276', '236', '350', '300', '1','M_PARCHBIG_WAITER','','','','');
+-- delete from machines where name = 'PARCHBIG_WAITER';
+-- INSERT INTO "main"."machines" ("id", "name", "view_id", "view_name", "left", "top", "right", "bottom", "local_visible", "dfa_name", "wip1_name", "wip2_name", "wip3_name", "wip4_name") 
+-- VALUES 
+-- ('15570', 'PARCHBIG_WAITER', '8710', 'IDV_PARCHBIG', '276', '236', '350', '300', '1','M_PARCHBIG_WAITER','','','','');
 
-delete from transitions where automaton =  'M_PARCHBIG_WAITER';
-INSERT INTO "main"."transitions" ("automaton", "state", "new_state", "opcode", "param_1", "param_2", "code")
-VALUES 
+-- delete from transitions where automaton =  'M_PARCHBIG_WAITER';
+-- INSERT INTO "main"."transitions" ("automaton", "state", "new_state", "opcode", "param_1", "param_2", "code")
+-- VALUES 
 
-('M_PARCHBIG_WAITER','0', 'waiting', 'Z_EPSILON', '', '', '
-        REF_MACHINE(SMP_EYEINFO);
-'),
-----
-('M_PARCHBIG_WAITER', 'waiting', 'showDiary', 'WAIT', '0', 'SIG_DIARY1', '
-        predicate diary(object,control,content);
-        diary(R_WPARM,?BPARM,?WPARM);
-        LOADVIEW(IDV_PARCHBIG);
-        SETTEXT(ID_PARCHBIGTXT,WPARM);
+-- ('M_PARCHBIG_WAITER','0', 'waiting', 'Z_EPSILON', '', '', '
+--         REF_MACHINE(SMP_EYEINFO);
+-- '),
+-- ----
+-- ('M_PARCHBIG_WAITER', 'waiting', 'showDiary', 'WAIT', '0', 'SIG_DIARY1', '
+--         predicate objectInfo(object,control,content);
+--         objectInfo(R_WPARM,?BPARM,?WPARM);
+--         LOADVIEW(IDV_PARCHBIG);
+--         SETTEXT(ID_PARCHBIGTXT,WPARM);
        
-'),
-('M_PARCHBIG_WAITER', 'showDiary', '0', 'Z_EPSILON', '', '', '');
------
+-- '),
+-- ('M_PARCHBIG_WAITER', 'showDiary', '0', 'Z_EPSILON', '', '', '');
+-- -----
 
------
+-- -----
--- data/sql/S0_EyeInfoV2.sql
+++ data/sql/S0_EyeInfoV2.sql
@@ -3,6 +3,29 @@ delete from games;
 
 ----RESOURCES 
 
+delete from views where [view_name] like 'IDV_PARCHBIG%';
+delete from views where [view_name] like 'IDV_PARCHSMALL%';
+INSERT INTO "main"."views" ("view_id", "view_name", "Z", "backgroundAudio", "locator_view", "behavior_id", "portal_filename", "surface_filename") 
+VALUES 
+('8710', 'IDV_PARCHBIG', '1', '0', '1', '2', 'wdepanel.vct', 'parchbig'),
+('8711', 'IDV_PARCHSMALL', '1', '0', '1', '2', 'wdepanel.vct', 'parchpan');
+
+
+delete from spr_names where name = 'IDS_PARCHBIGBK';
+INSERT INTO "main"."spr_names" ("name", "value", "id") 
+VALUES 
+('IDS_PARCHBIGBK', 'parchbigbk', '40650');
+--can use parchpanbk as control background
+
+
+delete from controls where id = 'ID_PARCHBIGTXT';
+delete from controls where id = 'ID_PARCHSMALLTXT';
+insert into controls values
+('IDV_PARCHBIG','ID_PARCHBIGTXT','LABEL','IDS_PARCHBIGBK','',46, 55, 0,'','', 'IDS_FONTTNB16', 0x010101,''),
+('IDV_PARCHSMALL','ID_PARCHSMALLTXT','LABEL','IDS_PARCHPANBK','',40, 20, 0,'','', 'IDS_FONTTNB16', 0x010101,'');
+
+
+
 delete from idv where [name] like 'IDV_SPELLP%';
 delete from views where [view_name] like 'IDV_SPELLP%';
 
@@ -30,85 +53,8 @@ INSERT INTO "main"."spr_names" ("name", "value", "id") VALUES
 ('IDS_REC_MEMSPELL', 'rec_memspell', '18810'),
 ('IDS_REC_KAMIOZA', 'rec_kamioza', '18811');
 
-delete from "main"."isa"  where [member] like 'IDD_ENCSTONE%';
-INSERT INTO "main"."isa" ("class", "member") VALUES ('ISA_ENCHANTEDSTONE', 'IDD_ENCSTONE');
-
-
-
-delete from idv where [name] like 'IDV_ENCHANTP%';
-delete from views where [view_name] like 'IDV_ENCHANTP%';
-
-INSERT INTO "main"."idv" ("name", "id") VALUES ('IDV_ENCHANTPAN', '8707');
-INSERT INTO "main"."views" ("view_id", "view_name", "Z", "backgroundAudio", "locator_view", "behavior_id", "portal_filename", "surface_filename") 
-VALUES ('8707', 'IDV_ENCHANTPAN', '1', '0', '1', '1', 'wdepanel.vct', 'PARCHPAN');
-
-delete from machines where name = 'SMP_EYEINFO';
-delete from transitions where [automaton] like 'M_EYEINF%';
-
-INSERT INTO "main"."machines" ("id", "name", "view_id", "view_name", "left", "top", "right", "bottom", "local_visible", "dfa_name", "wip1_name", "wip2_name", "wip3_name", "wip4_name") 
-VALUES 
-('112', 'SMP_EYEINFO', '1', 'IDV_MAIN_PANEL', '290', '12', '344', '64', '3', 'M_EYEINFO', '', '', '', '');
-
-
-INSERT INTO "main"."transitions" ("automaton", "state", "new_state", "opcode", "param_1", "param_2", "code") 
-VALUES 
-('M_EYEINFO', '0', '100', 'DROP', '0', '0', '
-PLAYWAVE(SOUND_POPUP);ASHOW(WOBJECT);
-MOV(BPARM,LVIEW);
-'),
-('M_EYEINFO', '0', 'replacedView', 'GRAB', '0', '0', '
-      SHOW(0);
-'),
-('M_EYEINFO', 'replacedView', '0', 'Z_EPSILON', '0', '0', '
-        LOADVIEW(BPARM);
-'),
-
-
-('M_EYEINFO', '100', '120', 'IS_A', 'WOBJECT', 'IDC_SCROLL', 
-       'MOV(WPARM,WOBJECT);
-        MAPi(WPARM,S12_SCROLL);
-        SIGNALi(SIG_SHOW,S12_SHIELD);
-        SIGNALi(SIG_SHOW,S12_WORD);
-        SIGNALi(SIG_SHOW,S12_ING1);
-        SIGNALi(SIG_SHOW,S12_ING2);
-        SIGNALi(SIG_SHOW,S12_ING3);
-        SIGNALi(SIG_SHOW,S12_ING4);
-        SIGNALi(SIG_SHOW,S12_NYST);
-       
-       '),
-('M_EYEINFO', '100', '130', 'IS_A', 'WOBJECT', 'ISA_ENCHANTEDSTONE', 
-        'MOV(WPARM,WOBJECT);      
-        '), 
-('M_EYEINFO', '100', '0', 'IS_A', 'WOBJECT', 'ISA_DIARY', 
-        'MOV(WPARM,WOBJECT);
-        SIGNAL(PARCHBIG_WAITER,SIG_DIARY1);
-        '),           
---This is where everything else would get mapped        
-
-('M_EYEINFO', '100', '0', 'IS_A', 'WOBJECT', 'IDC_NULL', '
-        MOV(WPARM,WOBJECT);
-'),   
-
-------------------------------------------------
-('M_EYEINFO', '100', '0', 'Z_EPSILON', '0', '0', ''),
-
-('M_EYEINFO', '120', '0', 'LOADVIEW', '0', 'IDV_SPELLPAN', ''),
-('M_EYEINFO', '130', '0', 'LOADVIEW', '0', 'IDV_ENCHANTPAN', '');
-
-
-
-
-
-delete from transitions where [automaton] like 'M_RECYCLE%';
-INSERT INTO "main"."transitions" ("automaton", "state", "new_state", "opcode", "param_1", "param_2", "code") VALUES 
-('M_RECYCLE', '0', '1', 'DROP', '0', '0', ''),
-('M_RECYCLE', '1', '0', 'VIDEO', '0', 'IDS_SMBURST', '');
-
-
 delete from machines where name = 'REC_%';
 delete from transitions where [automaton] like 'M_RECORD_%';
-
-
 INSERT INTO "main"."machines" ("id", "name", "view_id", "view_name", "left", "top", "right", "bottom", "local_visible", "dfa_name", "wip1_name", "wip2_name", "wip3_name", "wip4_name") 
 VALUES 
 
@@ -128,6 +74,7 @@ VALUES
 ('30013', 'REC_NYSTROMPAID', '8707', 'IDV_ENCHANTPAN', '120', '35', '130', '45', '3', 'M_RECORD_COUNTER', '', '', '', ''),
 ('30014', 'REC_BADKARMA', '8707', 'IDV_ENCHANTPAN', '130', '35', '140', '45', '3', 'M_RECORD_COUNTER', '', '', '', '');
 
+
 INSERT INTO "main"."transitions" ("automaton", "state", "new_state", "opcode", "param_1", "param_2", "code") 
 VALUES 
 ('M_RECORD_COMPLETE', '0', '100', 'WAIT','0', 'SIG_COMPLETE', 'ASSIGN(WSPRITE,WIP1);SHOW(WIP1);'),
@@ -135,6 +82,183 @@ VALUES
 ('M_RECORD_COMPLETE', '101', '0', 'Z_EPSILON','', '', 'CLEAR(WSPRITE);SHOW();'),
 
 ('M_RECORD_COUNTER', '0', '0', 'WAIT','0', 'SIG_COUNT', 'ADDi(WPARM,1);');
+
+
+
+---CREATE OBJECT CLASSES IF NEEDED
+
+delete from "main"."isa"  where [member] like 'IDD_ENCSTONE%';
+delete from isa where [class] like 'ISA_DIARY_%';
+INSERT INTO "main"."isa" ("class", "member") VALUES ('ISA_ENCHANTEDSTONE', 'IDD_ENCSTONE');
+INSERT INTO "main"."isa" ("class", "member") VALUES ('ISA_DIARY', 'IDD_DIARY1');
+
+
+
+delete from idv where [name] like 'IDV_ENCHANTP%';
+delete from views where [view_name] like 'IDV_ENCHANTP%';
+
+INSERT INTO "main"."idv" ("name", "id") VALUES ('IDV_ENCHANTPAN', '8707');
+INSERT INTO "main"."views" ("view_id", "view_name", "Z", "backgroundAudio", "locator_view", "behavior_id", "portal_filename", "surface_filename") 
+VALUES ('8707', 'IDV_ENCHANTPAN', '1', '0', '1', '1', 'wdepanel.vct', 'PARCHPAN');
+
+--------Recycle 
+
+delete from transitions where [automaton] like 'M_RECYCLE%';
+INSERT INTO "main"."transitions" ("automaton", "state", "new_state", "opcode", "param_1", "param_2", "code") VALUES 
+('M_RECYCLE', '0', '1', 'DROP', '0', '0', ''),
+('M_RECYCLE', '1', '0', 'VIDEO', '0', 'IDS_SMBURST', '');
+
+
+-- Machines for OBJECT Entries
+
+delete from machines where name = 'NIRET_DIARY_WAITER';
+INSERT INTO "main"."machines" ("id", "name", "view_id", "view_name", "left", "top", "right", "bottom", "local_visible", "dfa_name", "wip1_name", "wip2_name", "wip3_name", "wip4_name") 
+VALUES 
+('15570', 'NIRET_DIARY_WAITER', '8710', 'IDV_PARCHBIG', '276', '236', '350', '300', '1','M_GENERAL_WAITER','IDV_PARCHBIG','SIG_DIARY','ID_PARCHBIGTXT',''),
+('15571', 'OBJECT_WAITER', '8710', 'IDV_PARCHSMALL', '276', '236', '350', '300', '1','M_GENERAL_WAITER','IDV_PARCHSMALL','SIG_OBJECT','ID_PARCHSMALLTXT','');
+
+
+
+delete from transitions where automaton =  'M_DIARY_WAITER';
+delete from transitions where automaton =  'M_GENERAL_WAITER';
+INSERT INTO "main"."transitions" ("automaton", "state", "new_state", "opcode", "param_1", "param_2", "code")
+VALUES 
+
+('M_GENERAL_WAITER','0', 'waiting', 'Z_EPSILON', '', '', '
+        REF_MACHINE(SMP_EYEINFO);
+        CLEAR(WPARM);
+'),
+----
+('M_GENERAL_WAITER', 'waiting', 'showDiary', 'WAIT', '0', 'WIP2', '
+        predicate objectInfo(object,control,content);
+        objectInfo(R_WPARM,?BPARM,?WPARM);
+        LOADVIEW(WIP1);
+        SETTEXT(WIP3,WPARM);
+       
+'),
+('M_GENERAL_WAITER', 'showDiary', '0', 'Z_EPSILON', '', '', '');
+
+-----------------------------------------------------------------
+------------- DROP ON EYE AND CHECK
+delete from machines where name = 'SMP_EYEINFO';
+delete from transitions where [automaton] like 'M_EYEINF%';
+
+INSERT INTO "main"."machines" ("id", "name", "view_id", "view_name", "left", "top", "right", "bottom", "local_visible", "dfa_name", "wip1_name", "wip2_name", "wip3_name", "wip4_name") 
+VALUES 
+('112', 'SMP_EYEINFO', '1', 'IDV_MAIN_PANEL', '290', '12', '344', '64', '3', 'M_EYEINFO', '', '', '', '');
+
+
+INSERT INTO "main"."transitions" ("automaton", "state", "new_state", "opcode", "param_1", "param_2", "code") 
+VALUES 
+('M_EYEINFO', '0', 'checkObject', 'DROP', '0', '0', '
+PLAYWAVE(SOUND_POPUP);ASHOW(WOBJECT);
+MOV(BPARM,LVIEW);
+'),
+('M_EYEINFO', '0', 'replacedView', 'GRAB', '0', '0', '
+      SHOW(0);
+'),
+('M_EYEINFO', 'replacedView', '0', 'Z_EPSILON', '0', '0', '
+        LOADVIEW(BPARM);
+'),
+
+('M_EYEINFO', 'checkObject', 'viewSpell', 'IS_A', 'WOBJECT', 'IDC_SCROLL', 
+       'MOV(WPARM,WOBJECT);
+        MAPi(WPARM,S12_SCROLL);
+        SIGNALi(SIG_SHOW,S12_SHIELD);
+        SIGNALi(SIG_SHOW,S12_WORD);
+        SIGNALi(SIG_SHOW,S12_ING1);
+        SIGNALi(SIG_SHOW,S12_ING2);
+        SIGNALi(SIG_SHOW,S12_ING3);
+        SIGNALi(SIG_SHOW,S12_ING4);
+        SIGNALi(SIG_SHOW,S12_NYST);
+       
+       '),
+('M_EYEINFO', 'checkObject', 'viewEnchantment', 'IS_A', 'WOBJECT', 'ISA_ENCHANTEDSTONE', 
+        'MOV(WPARM,WOBJECT);      
+        '), 
+('M_EYEINFO', 'checkObject', '0', 'IS_A', 'WOBJECT', 'ISA_DIARY', 
+        'MOV(WPARM,WOBJECT);
+        if(WOBJECT == IDD_DIARY1 || WOBJECT == IDD_DIARY2){
+                 SIGNAL(NIRET_DIARY_WAITER,SIG_DIARY);
+        }
+        '),           
+     
+
+('M_EYEINFO', 'checkObject', '0', 'IS_A', 'WOBJECT', 'IDC_NULL', '
+        MOV(WPARM,WOBJECT);
+        SIGNAL(OBJECT_WAITER,SIG_OBJECT);
+'),   
+
+('M_EYEINFO', 'checkObject', '0', 'Z_EPSILON', '0', '0', ''),
+
+('M_EYEINFO', 'viewSpell', '0', 'LOADVIEW', '0', 'IDV_SPELLPAN', ''),
+('M_EYEINFO', 'viewEnchantment', '0', 'LOADVIEW', '0', 'IDV_ENCHANTPAN', '');
+
+
+
+
+
+--------------------Database Entries for Object text info display ( diarys and objects )
+
+
+
+--drop table if exists objectInfo;
+
+-- 'id' is used by the card index cartridge on the card disbursement machine in the city.
+-- create table objectInfo (object text, control text, content text);
+-- insert into objectInfo values
+-- ( 'IDD_DIARY1', 'ID_PARCHBIGTXT' ,'
+--         I leave this here in case something happens
+--         46 Zan, 192978
+        
+--         I Onam, archiologist of Niret 
+--         have  found the sacred map of our forefathers
+--         that grants power over the sky.
+        
+--         Although their language still comfounds me!
+
+--         Now, I shall continue the quest, spurred on 
+--         by our great discovery of a promise written in stone.
+--         I speak, of cours, of the stone rubbing unearthed 
+--         outside of Etnoc - that hideous city of great 
+--         sadness and tragedy.
+
+--         It is the Niret way of spirit that makes us so 
+--         much better than those of Etnoc.
+
+--         ....to take the seed of Azaria and plant it
+--          on the altar of seven moons.
+        
+--         I imagine the seed as some relic that existed 
+--         before the rains... I must find it! Something that
+--         contains this seed.
+
+--         I shall now see for myself and fill my spirit with joy - 
+--         Searching for some seed to plant on some altar!'),
+
+-- ( 'IDD_DIARY2', 'IDV_PARCHBIG' ,'Placeholder1'),
+-- ( 'IDD_DIARY3', 'IDV_PARCHBIG' ,'Placeholder2'),
+-- ( 'IDD_DIARY4', 'IDV_PARCHBIG' ,'Placeholder3'),
+-- ( 'IDD_BLOBBALL', 'IDV_PARCHSMALL' ,'
+
+--        The object of Blob ball is to get rid of all your balls first.
+--        Pick a side and roll a die. place your ball in the number
+--        corresponding to the number rolled.  If you roll a six, that is 
+--        great because your ball gets sucked away. 
+       
+--        At any point, you can pass your turn to the other player 
+--        If you roll a number which already has a ball in it, 
+--        you take it and the other player takes over.
+-- '),
+-- ( 'IDD_EMERALD', 'IDV_PARCHSMALL' ,'
+--        The Emerald of Light! 
+--        Rathe will be ecstatic.
+-- '),
+-- ( 'IDD_RUBY', 'IDV_PARCHSMALL' ,'
+--         This is a Ruby. It may have some
+--         value to somebody. It may not.
+-- ');
+
 
 
 
