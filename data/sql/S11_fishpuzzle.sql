fbf2296760c06aa53108d9264be56923901debbf
Rick Gonzalez <rick.gonzalez68@gmail.com>
2024-09-26 19:54:33 -0400

    some small fixes for fishpuzzle , spells and sanddirt sizes

--- data/sql/S11_fishpuzzle.sql
+++ data/sql/S11_fishpuzzle.sql
@@ -14,11 +14,12 @@ delete from machines where name = 'S11_SHOREBIN';
 delete from machines where name = 'S11_RIPPLE';
 
 delete from transitions where automaton = 'M_TINYFISH';
-
+delete from transitions where automaton = 'M_LUNCHPORTAL';
+delete from transitions where automaton = 'M_XANIPORTAL';
 
 delete from objects where object = 'IDD_LUNCHCRUMBS';
 insert into objects values
-('IDD_LUNCHCRUMBS',0x3e8a,'ISA_BAIT','LUNCHCRUMBS','LUNCHCRUMBS','LUNCHCRUMBS');
+('IDD_LUNCHCRUMBS','ISA_BAIT','LUNCHCRUMBS','LUNCHCRUMBS','LUNCHCRUMBS');
 
 
 
@@ -28,24 +29,31 @@ VALUES
 ('IDS_RIPPLE', 'ripple', '16011');
 
 INSERT INTO "main"."idv" ("name", "id") VALUES ('IDV_LUNCHCU', '4361');
-INSERT INTO "main"."views" ("view_id", "view_name", "Z", "backgroundAudio", "locator_view", "behavior_id", "portal_filename", "surface_filename") 
+INSERT INTO "main"."views" ("view_name", "Z", "backgroundAudio", "locator_view", "behavior_id", "portal_filename", "surface_filename") 
 VALUES 
-('4361', 'IDV_LUNCHCU', '1', '1', '1', '1', 'smlpanel.vct', 'LUNCHCU');
+('IDV_LUNCHCU', '1', '1', '1', '1', 'smlpanel.vct', 'LUNCHCU');
 
 INSERT INTO "main"."panel_nav" ("from", "forward", "back") VALUES ('IDV_LUNCHCU', '', 'IDV_FH1PAN');
 
 
-INSERT INTO "main"."machines" ("id", "name", "view_id", "view_name", "left", "top", "right", "bottom", "local_visible", "dfa_name", "wip1_name", "wip2_name", "wip3_name", "wip4_name")
+INSERT INTO "main"."machines" ("name", "view_name", "left", "top", "right", "bottom", "local_visible", "dfa_name", "wip1_name", "wip2_name", "wip3_name", "wip4_name")
 VALUES 
-('4465', 'S11_RIPPLE', '4356', 'IDV_FH1PAN', '988', '265', '1057', '299', '2', 'M_QANIPORTAL', 'IDS_RIPPLE', '', 'IDV_SHORE', ''),
-('4464', 'S11_LUNCHCRUMBS', '4361', 'IDV_LUNCHCU', '139', '139', '213', '216', '2', 'M_BIN', 'IDD_LUNCHCRUMBS', '', '', ''),
-('4463', 'S11_LUNCH', '4356', 'IDV_FH1PAN', '2149', '134', '2259', '201', '2', 'M_LUNCHPORTAL', 'IDV_LUNCHCU', 'IDV_LUNCHCU', '60', ''),
-('4461', 'S11_SHOREBIN', '4360', 'IDV_SHORE', '129', '87', '321', '213', '0', 'M_TINYFISH', 'IDS_FISH', 'IDD_BAIT3', '60','');
+('S11_RIPPLE', 'IDV_FH1PAN', '988', '265', '1057', '299', '2', 'M_XANIPORTAL', 'IDS_RIPPLE', '', 'IDV_SHORE', ''),
+('S11_LUNCHCRUMBS', 'IDV_LUNCHCU', '139', '139', '213', '216', '2', 'M_BIN', 'IDD_LUNCHCRUMBS', '', '', ''),
+('S11_LUNCH', 'IDV_FH1PAN', '2149', '134', '2259', '201', '2', 'M_LUNCHPORTAL', 'IDV_LUNCHCU', 'IDV_LUNCHCU', '60', ''),
+('S11_SHOREBIN', 'IDV_SHORE', '129', '87', '321', '213', '0', 'M_TINYFISH', 'IDS_FISH', 'IDD_BAIT3', '60','');
 
 INSERT INTO "main"."transitions" ("automaton", "state", "new_state", "opcode", "param_1", "param_2", "code") 
 VALUES 
 ('M_LUNCHPORTAL', '0', '1', 'CLICK', '0', '0', ''),
-('M_LUNCHPORTAL', '1', '0', 'LOADVIEW', '0', 'IDV_LUNCHCU', '');
+('M_LUNCHPORTAL', '1', '0', 'LOADVIEW', '0', 'IDV_LUNCHCU', ''),
+
+
+
+ ('M_XANIPORTAL', '0', '1', 'MOV', 'WSPRITE', 'WIP1',''),
+ ('M_XANIPORTAL', '1', '2', 'ASHOW', 'WSPRITE', 'V_LOOP',''),
+ ('M_XANIPORTAL', '2', '3', 'CLICK', '0', '0',''),
+ ('M_XANIPORTAL', '3', '1', 'LOADVIEW', '0', 'WIP3','');
 
 
 
